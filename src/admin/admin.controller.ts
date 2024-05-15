d4a80d19a7b4bae40c3e530bf58ae73d01aa401b
suk-6 <me@suk.kr>
2024-05-16 02:15:50 +0900

    chore: Add pendings, approve, reject, door-status, lock, unlock, and restrict endpoints to AdminController

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -5,8 +5,46 @@ import { AdminService } from './admin.service';
 export class AdminController {
     constructor(private readonly adminService: AdminService) {}
 
+    @Get('pendings')
+    async getPendings(@Query('code') code: string) {
+        return this.adminService.getPendings(code);
+    }
+
+    @Get('approve')
+    async approveUser(
+        @Query('code') code: string,
+        @Query('id') userId: string,
+    ) {
+        return this.adminService.approveUser(code, userId);
+    }
+
+    @Get('reject')
+    async rejectUser(@Query('code') code: string, @Query('id') userId: string) {
+        return this.adminService.rejectUser(code, userId);
+    }
+
     @Get('access-logs')
     async accessLogs(@Query('code') code: string) {
         return this.adminService.accessLogs(code);
+    }
+
+    @Get('door-status')
+    async doorStatus() {
+        return this.adminService.getDoorStatus();
+    }
+
+    @Get('lock')
+    async lock(@Query('code') code: string) {
+        return this.adminService.lockDoor(code);
+    }
+
+    @Get('unlock')
+    async unlock(@Query('code') code: string) {
+        return this.adminService.unlockDoor(code);
+    }
+
+    @Get('restrict')
+    async restrict(@Query('code') code: string) {
+        return this.adminService.restrictDoor(code);
     }
 }
