9f994398d8ece045f38569ffbf09ea3efbb8f0f5
suk-6 <me@suk.kr>
2024-05-16 02:15:54 +0900

    chore: Add pendings, approve, reject, door-status, lock, unlock, and restrict endpoints to AdminController

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,5 +1,10 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { DoorService } from 'src/door/door.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -7,7 +12,60 @@ export class AdminService {
     constructor(
         private readonly prisma: PrismaService,
         private readonly authService: AuthService,
+        private readonly doorService: DoorService,
     ) {}
+
+    async getPendings(code: string) {
+        const admin = await this.authService.checkAdmin(code);
+        if (!admin) throw new UnauthorizedException('Not Admin');
+
+        const pendings = await this.prisma.user.findMany({
+            where: {
+                isApproved: false,
+            },
+        });
+
+        return pendings.map((pending) => ({
+            id: pending.id,
+            name: pending.name,
+            studentId: pending.studentId,
+        }));
+    }
+
+    async approveUser(code: string, userId: string) {
+        const admin = await this.authService.checkAdmin(code);
+        if (!admin) throw new UnauthorizedException('Not Admin');
+
+        const user = await this.authService.getUserById(userId);
+        if (!user) throw new BadRequestException('Invalid User');
+
+        await this.prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                isApproved: true,
+            },
+        });
+
+        return { status: true };
+    }
+
+    async rejectUser(code: string, userId: string) {
+        const admin = await this.authService.checkAdmin(code);
+        if (!admin) throw new UnauthorizedException('Not Admin');
+
+        const user = await this.authService.getUserById(userId);
+        if (!user) throw new BadRequestException('Invalid User');
+
+        await this.prisma.user.delete({
+            where: {
+                id: userId,
+            },
+        });
+
+        return { status: true };
+    }
 
     async accessLogs(code: string) {
         if (!(await this.authService.checkAdmin(code))) {
@@ -28,9 +86,46 @@ export class AdminService {
                 return {
                     studentId: user.studentId,
                     name: user.name,
-                    accessTime: log.createdAt.getTime(),
+                    timestamp: log.createdAt,
                 };
             }),
         );
+    }
+
+    async getDoorStatus() {
+        return { status: await this.doorService.getDoorStatus() };
+    }
+
+    async lockDoor(code: string) {
+        if (!(await this.authService.checkAdmin(code))) {
+            throw new UnauthorizedException(
+                'You are not authorized to access this resource',
+            );
+        }
+
+        await this.doorService.lockDoor();
+        return { status: true };
+    }
+
+    async unlockDoor(code: string) {
+        if (!(await this.authService.checkAdmin(code))) {
+            throw new UnauthorizedException(
+                'You are not authorized to access this resource',
+            );
+        }
+
+        await this.doorService.unlockDoor();
+        return { status: true };
+    }
+
+    async restrictDoor(code: string) {
+        if (!(await this.authService.checkAdmin(code))) {
+            throw new UnauthorizedException(
+                'You are not authorized to access this resource',
+            );
+        }
+
+        await this.doorService.restrictDoor();
+        return { status: true };
     }
 }
