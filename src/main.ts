58a8fe6f1c7172c6041023c133d7dd2477584c46
suk-6 <me@suk.kr>
2024-05-16 11:35:20 +0900

    chore: Update CORS origin to allow only 'https://ams.make.dyhs.kr'

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: true,
+        origin: ['https://ams.make.dyhs.kr'],
         credentials: true,
     });
     await app.listen(3001);
