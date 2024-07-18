ff2fd4ba5ac8795fb5b0c00c1c98bebc83cb84c5
suk-6 <me@suk.kr>
2024-07-18 15:31:29 +0900

    feat: Add ScheduleModule to DoorModule for cron jobs

--- src/door/door.module.ts
+++ src/door/door.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { DoorService } from './door.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Global()
 @Module({
+    imports: [ScheduleModule.forRoot()],
     providers: [DoorService],
     exports: [DoorService],
 })
